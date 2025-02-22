7c9cfc2960e8ee28fe3c9d9675155b382ed98882
asier <yeabsra143@gmail.com>
2025-02-23 00:41:19 +0300

    modified:   app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
 <script setup>
 const route = useRoute();
 const { trackReferral } = useReferral();
+import { useRouter } from "vue";
+
+const router = useRouter();
+const loading = useLoadingIndicator();
+
+router.beforeEach(() => {
+  loading.start();
+});
+
+router.afterEach(() => {
+  loading.finish();
+});
 
 onMounted(async () => {
   const refCode = route.query.ref;
